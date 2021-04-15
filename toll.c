eaba4ed8b6b437bec87519ff9c48a0dca99c1934
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-04-15 15:49:07 -0500

    Update toll.c

--- toll.c
+++ toll.c
@@ -76,48 +76,7 @@ int _strlen(char *s)
 	return (counter);
 }
 
-int _execvp(char *name, char *argv[])
-{
-	char *path = pEnv("PATH");
-	char fullName[MAX_NAME_SIZE + 1];
-	char *first = NULL, *last = NULL;
-	int size = 0, noAccess = 0;
 
-	if (_strchr(name, '/') != 0)
-	{
-		execArgs(name, argv);
-		exit(127);
-	}
-	if (!path)
-		path = "/usr/local/bin:/bin:/usr/bin:/usr/local/games:/usr/games";
-	for (first = path; ; first = last + 1)
-	{
-		for (last = first; (*last != 0) && (*last != ':'); last++)
-		{
-		}
-		size = last - first;
-		_strncpy(fullName, first, size);
-		if (last[-1] != '/')
-			fullName[size++] = '/';
-		_strcpy(fullName + size, name);
-		execArgs(fullName, argv);
-		if (errno == EACCES)
-		{
-			noAccess = 1;
-		}
-		else if (errno != ENOENT)
-			break;
-		if (*last == 0)
-		{
-			if (noAccess)
-			{
-				errno = EACCES;
-			}
-			break;
-		}
-	}
-	return (-1);
-}
 
 char *pEnv(char *name)
 {
@@ -138,41 +97,3 @@ char *pEnv(char *name)
 	}
 	return (NULL);
 }
-
-void execArgs(char *file, char *argv[])
-{
-	int i = 0;
-	char *newArgv[MAX_ARGS + 1];
-
-	_execve(file, argv, environ);
-
-	if (errno == ENOEXEC)
-	{
-		for (i = 0; argv[i]; i++)
-		{
-		}
-		if (i >= MAX_ARGS)
-		{
-			exit(EXIT_FAILURE);
-		}
-		newArgv[0] = "sh";
-		newArgv[1] = file;
-		for (i = 1; argv[i] != 0; i++)
-		{
-			newArgv[i + 1] = argv[i];
-		}
-		newArgv[i + 1] = NULL;
-		_execve("/bin/sh", newArgv, environ);
-	}
-}
-/**
- * __execvp - implentation of the "execvp" function
- *
- * @name: char pointer
- * @argv: argument vector
- * * * * *
- * ENOENT - No such file or directory (POSIX.1-2001)
- * EACCES - Permission denied (POSIX.1-2001)
- * * * * *
- * Return: -1
- */

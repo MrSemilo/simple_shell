80a5ebdbf76c893fd66ade6a7c374563f3395689
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-04-15 15:25:28 -0500

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -1,6 +1,10 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
+#define SIZE 1024
+#define MAX_NAME_SIZE 1000
+#define MAX_ARGS 1000
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -20,5 +24,9 @@ int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
+int _execvp(char *name, char *argv[]);
+char *_strchr(char *s, char c)
+char *pEnv(char *name);
+void execArgs(char *file, char *argv[]);
 
 #endif

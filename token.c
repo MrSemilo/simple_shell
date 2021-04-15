6dc40ad937a7e5d8341d1769043fae4af7a19d14
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-04-15 15:47:57 -0500

    Update token.c

--- token.c
+++ token.c
@@ -30,6 +30,85 @@ void espacio(char *line, char **token)
  *
  *Return: return (1).
  */
+void execArgs(char *file, char *argv[])
+{
+	int i = 0;
+	char *newArgv[MAX_ARGS + 1];
+
+	execve(file, argv, environ);
+
+	if (errno == ENOEXEC)
+	{
+		for (i = 0; argv[i]; i++)
+		{
+		}
+		if (i >= MAX_ARGS)
+		{
+			exit(EXIT_FAILURE);
+		}
+		newArgv[0] = "sh";
+		newArgv[1] = file;
+		for (i = 1; argv[i] != 0; i++)
+		{
+			newArgv[i + 1] = argv[i];
+		}
+		newArgv[i + 1] = NULL;
+		execve("/bin/sh", newArgv, environ);
+	}
+}
+/**
+ * __execvp - implentation of the "execvp" function
+ *
+ * @name: char pointer
+ * @argv: argument vector
+ * * * * *
+ * ENOENT - No such file or directory (POSIX.1-2001)
+ * EACCES - Permission denied (POSIX.1-2001)
+ * * * * *
+ * Return: -1
+ */
+int __execvp(char *name, char *argv[])
+{
+	char *path = pEnv("PATH");
+	char fullName[MAX_NAME_SIZE + 1];
+	char *first = NULL, *last = NULL;
+	int size = 0, noAccess = 0;
+
+	if (_strchr(name, '/') != 0)
+	{
+		execArgs(name, argv);
+		exit(127);
+	}
+	if (!path)
+		path = "/usr/local/bin:/bin:/usr/bin:/usr/local/games:/usr/games";
+	for (first = path; ; first = last + 1)
+	{
+		for (last = first; (*last != 0) && (*last != ':'); last++)
+		{
+		}
+		size = last - first;
+		_strncpy(fullName, first, size);
+		if (last[-1] != '/')
+			fullName[size++] = '/';
+		_strcpy(fullName + size, name);
+		execArgs(fullName, argv);
+		if (errno == EACCES)
+		{
+			noAccess = 1;
+		}
+		else if (errno != ENOENT)
+			break;
+		if (*last == 0)
+		{
+			if (noAccess)
+			{
+				errno = EACCES;
+			}
+			break;
+		}
+	}
+	return (-1);
+}
 int ejecutar(char **token, char *var2)
 {
 	pid_t pid;

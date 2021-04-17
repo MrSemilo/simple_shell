656e32b5b4703878a8306df69699542b4d7ed47f
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-04-16 20:33:15 -0500

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -28,7 +28,7 @@ char *_strchr(char *s, char c);
 char *pEnv(char *name);
 void execArgs(char *file, char *argv[]);
 char *_strncpy(char *dest, char *src, int n);
-int compr_avanz(char **tokens);
+int compr_avanz(char **tokens, char *var2);
 void _env(char *tok[]);
 int _cd(char *tok[]);
 void errors(char __attribute__((unused)) **error);

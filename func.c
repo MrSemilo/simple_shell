7bbd33018aef0bbd67f5077478c699dfa23f56f9
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-04-16 20:01:52 -0500

    Update func.c

--- func.c
+++ func.c
@@ -83,3 +83,22 @@ char *_strncpy(char *dest, char *src, int n)
 		*dest++ = '\0';
 	return (ptr);
 }
+/**
+ * _strcmp - compares two strings.
+ * @s1: Destiny
+ * @s2: source
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
